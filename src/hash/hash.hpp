6e6be08e913cc546199f63cf8f75ab7591c54830
Randy Horman <rwhorman@gmail.com>
2019-01-07 23:02:58 -0500

    generic hash definition

---
+++ src/hash/hash.hpp
@@ -0,0 +1,32 @@
+#ifndef __HASH_HPP__
+#define __HASH_HPP__ 1
+
+#include <array>
+#include <cstddef>
+#include <string_view>
+
+namespace crypto {
+
+template <class Algorithm> class hash {
+public:
+  using digest_t = std::array<std::byte, Algorithm::digest_size_bytes>;
+
+  hash &operator<<(std::string_view in_data) {
+    _algorithm.hash_data(in_data);
+    return *this;
+  };
+
+  hash &operator>>(digest_t &out_digest) {
+    out_digest = _algorithm.get_digest();
+    return *this;
+  };
+
+  void reset() { _algorithm.reset(); };
+
+private:
+  Algorithm _algorithm;
+};
+
+} // namespace crypto
+
+#endif
