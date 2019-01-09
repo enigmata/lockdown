eb7165a2177d3f4b49b04b9b99ca11440758601d
Randy Horman <rwhorman@gmail.com>
2019-01-08 20:30:45 -0500

    initialize hash with prescribed bytes

--- src/hash/sha256/sha256.hpp
+++ src/hash/sha256/sha256.hpp
@@ -9,23 +9,30 @@
 namespace crypto::sha256 {
 
 const std::size_t BLOCK_SIZE_BYTES = 64;
+const std::size_t DIGEST_SIZE_BYTES = 32;
+const std::size_t DIGEST_SIZE_UINT32 =
+    DIGEST_SIZE_BYTES / sizeof(std::uint32_t);
+const std::array<std::uint32_t, DIGEST_SIZE_UINT32> DIGEST_INITIAL_VALS{
+    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
+    0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19};
 
 class hash_algorithm {
 public:
-  static const std::size_t digest_size_bytes = 32;
+  static const std::size_t digest_size_bytes = DIGEST_SIZE_BYTES;
 
   using digest_t = std::array<std::byte, digest_size_bytes>;
+
+  hash_algorithm(void) { reset(); };
 
   void hash_data(std::string_view){};
 
   digest_t get_digest(void);
 
-  void reset(){};
+  void reset(void) { _digest_wip = DIGEST_INITIAL_VALS; };
 
 private:
   std::array<std::byte, BLOCK_SIZE_BYTES> _underflow;
-  std::array<std::uint32_t, digest_size_bytes / sizeof(std::uint32_t)>
-      _digest_wip;
+  std::array<std::uint32_t, DIGEST_SIZE_UINT32> _digest_wip;
 };
 
 } // namespace crypto::sha256
